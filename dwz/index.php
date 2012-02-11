296f4813ae77f3c4c5dc793ad15535df5e7371de
b5617777 <b5617777@gmail.com>
2012-02-11 02:21:09 +0000

    bs6mq2Gy7yG9

---
+++ dwz/index.php
@@ -0,0 +1,694 @@
+<html><head><title>网址缩短器</title>
+<style type="text/css">
+* {
+	margin: 0;
+	padding: 0;
+}
+#createformlabel {
+	text-align: center;
+}
+.smallink {
+	font-size: 11px;
+}
+.biglink {
+	font-size: 22px;
+}
+.ioslink {
+	font-size: 30px;
+}
+.tip {
+        padding-top: 10px;
+        font-size: 10px;
+}
+input.tb {
+        width: 480px;
+        font-size: 20px;
+        font-family: verdana, serif;
+}
+img.menuimg {
+	float: left;
+	border: 0;
+}
+img.graph {
+	border: 2px solid #000;
+	margin-bottom: 10px;
+}
+img.floatRight {
+	float: right;
+	margin: 4px;
+	border: 0;
+}
+img.floatLeft {
+	float: right;
+	margin: 4px;
+	border: 0;
+}
+#wrapper {
+	min-height: 100%;
+	height: auto !important;
+	height: 100%;
+	margin: 0 auto -65px;
+	background: #fff;
+	background: -moz-linear-gradient(top, #fff, #dfd);
+	background: -webkit-gradient(linear, center top, center bottom, from(#fff), to(#dfd)); 
+}
+img.logo {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        padding-bottom: 10px;
+        padding-top: 10px;
+}
+#frontpage form {
+	padding: 0;
+	margin: 0;
+}
+
+#main {
+	position: relative;
+	text-align: left;
+	margin: 5px;
+	font-size: 14px;
+}
+#main blockquote {
+	font-family: serif;
+	font-size: 12pt;
+	padding: 0;
+	margin: 0;
+	margin-top: 5px;
+	margin-bottom: 5px;
+	margin-left: 30px;
+}
+#main h2 {
+	font-size: 26px;
+}
+#main h4 {
+	font-size: 20px;
+}
+#main form {
+        padding: 0;
+        margin: 0;
+}
+#menu img {
+	border: 0;
+	margin-top: 0px;
+	vertical-align: bottom;
+}
+#main ul {
+        list-style-type: disc;
+	margin-left: 40px;
+	font-size: 14px;
+	padding-bottom: 10px;
+}
+h4 {
+	padding-bottom: 5px;
+	padding-top: 5px;
+}
+#menu {
+	position: relative;
+	width: 100%;
+        font-size: 16px;
+        font-family: verdana, serif;
+	text-align: left;
+	padding-bottom: 1px;
+	border-color: #0b0;
+	border-bottom-width: 1px;
+	border-bottom-style: solid;
+}
+#menu a {
+	bottom: 0;
+	margin: 0;
+	padding: 0;
+	padding-left: 2px;	
+	padding-right: 18px;
+}
+#menu a:link {
+	text-decoration: none;
+	color: #000;
+}
+#menu a:visited {
+	text-decoration: none;
+	color: #000;
+}
+#menu a:hover {
+	text-decoration: none;
+	color: #0b0;
+}
+#errortext {
+	color: red;
+	text-align: center;
+	font-size: 15px;
+}
+#shorturllabel {
+	width: 445px;
+        display: none;
+        margin-left: auto;
+        margin-right: auto;
+	position: relative;
+	font-size: 10px;
+	font-family: verdana, serif;
+	text-align:right;
+}
+#shorturllabel2 {
+	width: 445px;
+	display: none;
+        margin-left: auto;
+        margin-right: auto;
+        position: relative;
+	text-align:right;
+        font-size: 10px;
+        font-family: verdana, serif;
+}
+#shorturllabel3 {
+        width: 445px;
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        position: relative;
+        text-align:right;
+        font-size: 10px;
+        font-family: verdana, serif;
+}
+#urlboxcontainer {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+	margin-bottom: 0px;
+        height: 30px;
+        width: 450px;
+	text-align: center;
+}
+input.urlbox {
+	margin: 0;
+	width: 440px;
+	font-size: 20px;
+	font-family: verdana, serif;
+}
+input.shorturlbox {
+	margin: 0;
+	font-size: 14px;
+	font-family: verdana, serif;
+	width: 285px;
+}
+#shorturlboxcontainer {
+        display: none;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 0px;
+	margin-bottom: 5px;
+	padding-left: 5px;
+        height: 20px;
+        width: 400px;
+        text-align: left;
+        font-family: verdana, serif;
+	font-size: 14px;
+}
+#options {
+        display: none;
+        margin-left: auto;
+        margin-right: auto;
+        font-family: verdana, serif;
+        font-size: 12px;
+	text-align: center;
+}
+#statsopt {
+        display: none;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 4px;
+        margin-bottom: 4px;
+        font-family: verdana, serif;
+        font-size: 12px;
+        text-align: center;
+}
+#options input {
+	margin-left: 4px;
+	margin-right: 2px;
+}
+input.submitbutton {
+	margin-bottom: 10px;
+	display: block;
+	margin-left: auto;
+	margin-right: auto;
+	width: 100px;
+	font-size: 16px;
+}
+html, body {
+	height: 100%;
+	font-family: verdana, serif;
+	min-width: 540px;
+}
+body hr {
+        border-top: 1px solid #0b0;
+        height: 1px;
+        width: 100%;
+	margin-top: 5px;
+	margin-bottom: 5px;
+}
+body h1 {
+	margin-bottom: 12px;
+}
+body h2 {
+	margin-bottom: 12px;
+}
+body h3 {
+	margin-bottom: 12px;
+}
+body p {
+	margin-bottom: 15px;
+}
+body a:link {
+        text-decoration: underline;
+        color: #070;
+}
+body a:visited {
+        text-decoration: underline;
+        color: #070;
+}
+body a:hover {
+        text-decoration: underline;
+        color: #0b0;
+}
+body {
+	text-align: center;
+	padding: 0;
+	margin: 0;
+}
+#hosting {
+        text-align: center;
+        font-size: 11px;
+}
+#notice {
+        text-align: center;
+        font-size: 12px;
+	margin-top: 8px;
+	margin-bottom: 8px;
+}
+body span.reg {
+        font-size: 10px;
+}
+body a.memsetlink:link {
+        text-decoration: none;
+        color: #070;
+        font-family: "arial black", serif;
+        font-size: 14px;
+	font-weight: bold;
+}
+body a.memsetlink:visited {
+        text-decoration: none;
+        color: #070;
+        font-family: "arial black", serif;
+        font-size: 14px;
+	font-weight: bold;
+}
+body a.memsetlink:hover {
+        text-decoration: none;
+        color: #0b0;
+        font-family: "arial black", serif;
+        font-size: 14px;
+	font-weight: bold;
+}
+#disabled {
+	padding: 10px;
+	margin: 0px;
+	background: #a00;	
+	border-style:solid;
+	border-width: 10px;
+	border-color: black;
+	color: #fff;
+	text-align: left;
+}
+#disabled h2 {
+	text-align: center;
+}
+#disabled a:link {
+        text-decoration: underline;
+        color: #fff;
+}
+#disabled a:visited {
+        text-decoration: underline;
+        color: #fff;
+}
+#disabled a:hover {
+        text-decoration: underline;
+        color: #faa;
+}
+.question {
+	font-size: 14px;
+	font-weight: bold;
+	color: #070;
+}
+.answer {
+	font-size: 14px;
+	font-style: italic;
+}
+.newspost {
+	border:3px solid #505050;
+	margin-bottom: 4px;
+}
+.newsdate {
+	font-style: italic;
+	font-size: 14px;
+	float: right;
+	margin: 2px;
+}
+.newstitlebar {
+	background-color: #5f5;
+	padding: 4px;
+}
+.newsheadline {
+	font-weight: bold;
+	font-size: 24px;
+}
+.newscontent {
+	margin: 2px;
+	font-size: 14px;
+	font-weight: normal;
+}
+.newsposter {
+	font-weight: normal;
+	font-style: italic;
+	font-size: 10px;
+	color: #090;
+	margin: 2px;
+}
+#reftable {
+	border: 2px solid black;
+	min-width: 400px;
+}
+#reftable th {
+	background-color:#88ff88;
+	padding: 6px;
+}
+#reftable td {
+	border: 1px solid black;
+	padding: 2px;
+}
+#statsmenu {
+	font-size: 12px;
+	width: 560px;
+}
+#statsmenu a {
+        padding-left: 2px;
+        padding-right: 12px;
+}
+#qrlabel {
+	font-size: 11px;
+	margin-top: 15px;
+}
+#origurl {
+	font-size: 11px;
+	margin-top: 15px;
+}
+#qrmessage {
+        font-size: 11px;
+        margin-top: 15px;
+	display: none;
+}
+#count {
+	text-align: center;
+	font-size: 12px;
+	margin-bottom: 10px;
+}
+#minisculetext {
+	font-size: 4px;
+}
+#recent {
+        text-align: center;
+        font-size: 12px;
+	margin-bottom: 10px;
+}
+</style>
+</head><body><br /><br /><br /><br /><br /><div id="wrapper"><div id="main"><img class="logo" alt="超级短网址" src="Logo.png">
+
+<?php
+//URL-HTTP判断 (1-正确 0-错误)
+function is_httpurl($str){
+  return preg_match("/^http:\/\/[A-Za-z0-9]+\.[A-Za-z0-9]+[\/=\?%\-&_~`@[\]\':+!]*([^<>\"])*$/", $str);
+ }
+//URL-FTP判断 (1-正确 0-错误) 
+function is_ftpurl($str){
+  return preg_match("/^ftp:\/\/[A-Za-z0-9]+\.[A-Za-z0-9]+[\/=\?%\-&_~`@[\]\':+!]*([^<>\"])*$/", $str);
+} 
+//GBK长度计算
+function abslength($str){
+    $len=strlen($str);
+    $i=0;
+    while($i<$len)
+    {
+        if(preg_match("/^[".chr(0xa1)."-".chr(0xff)."]+$/",$str[$i]))
+        {
+            $i+=2;
+        }
+        else
+        {
+            $i+=1;
+        }
+    }
+    return $i;
+} 
+//UTF8长度计算
+function strlen_utf8($str) {
+$i = 0;
+$count = 0;
+$len = strlen ($str);
+while ($i < $len) {
+$chr = ord ($str[$i]);
+$count++;
+$i++;
+if($i >= $len) break;
+if($chr & 0x80) {
+$chr <<= 1;
+while ($chr & 0x80) {
+$i++;
+$chr <<= 1;
+}
+}
+}
+return $count;
+}
+//JSON to Array
+function object_array($array)
+{
+   if(is_object($array))
+   {
+    $array = (array)$array;
+   }
+   if(is_array($array))
+   {
+    foreach($array as $key=>$value)
+    {
+     $array[$key] = object_array($value);
+    }
+   }
+   return $array;
+}
+//BaiDu
+function baidu($xxurl){
+
+$ch=curl_init();
+
+curl_setopt($ch,CURLOPT_URL,"http://dwz.cn/create.php");
+
+curl_setopt($ch,CURLOPT_POST,true);
+
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+
+$data=array('url'=>$xxurl);
+
+curl_setopt($ch,CURLOPT_POSTFIELDS,$data);
+
+$strRes=curl_exec($ch);
+
+curl_close($ch);
+
+$arrResponse=json_decode($strRes,true);
+
+if($arrResponse['status']==0)
+
+return $arrResponse['tinyurl'];
+
+}
+//Bai.LU
+function bailu($xxurl){
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "http://bai.lu/api?url=" . urlencode($xxurl));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+$url = json_decode(curl_exec($ch), true);
+echo $url['url'];
+}
+//Bit.LY
+function bitly($xxurl){
+
+function get_bitly_short_url($url,$login,$appkey,$format='txt') {  
+$connectURL = 'http://api.bit.ly/v3/shorten?login='.$login.'&apiKey='.$appkey.'&uri='.urlencode($url).'&format='.$format;  
+return curl_get_result($connectURL);  
+}  
+ 
+function curl_get_result($url) {  
+$ch = curl_init();  
+$timeout = 5;  
+curl_setopt($ch,CURLOPT_URL,$url);  
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);  
+curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);  
+$data = curl_exec($ch);  
+curl_close($ch);  
+return $data;  
+}  
+  
+return $short_url = get_bitly_short_url($xxurl,'cl17726','R_8b11d4a89732abac4fb46ef9b40f54ec');  
+
+}
+//is.GD
+function isgd($xxurl){
+  $f = new SaeFetchurl();
+  $content = $f->fetch('http://is.gd/create.php?format=simple&url='.$xxurl);
+return $content;
+}
+//v.GD
+function vgd($xxurl){
+  $f = new SaeFetchurl();
+  $content = $f->fetch('http://v.gd/create.php?format=simple&url='.$xxurl);
+return $content;
+}
+//LNK.by
+function lnkby($xxurl){
+$f = new SaeFetchurl();
+$content = $f->fetch('http://lnk.by/Shorten?url='.$xxurl.'&format=json');
+$data=object_array(json_decode($content));
+return ($data['ShortUrl']);
+} 
+//Sinaweibo
+function sinaweibo($xxurl){
+function shortenSinaUrl($long_url){
+ $apiKey='4098049467';
+ $apiUrl='http://api.t.sina.com.cn/short_url/shorten.json?source='.$apiKey.'&url_long='.$long_url;
+ $curlObj = curl_init();
+ curl_setopt($curlObj, CURLOPT_URL, $apiUrl);
+ curl_setopt($curlObj, CURLOPT_RETURNTRANSFER, 1);
+ curl_setopt($curlObj, CURLOPT_SSL_VERIFYPEER, 0);
+ curl_setopt($curlObj, CURLOPT_HEADER, 0);
+ curl_setopt($curlObj, CURLOPT_HTTPHEADER, array('Content-type:application/json'));
+ $response = curl_exec($curlObj);
+ curl_close($curlObj);
+ $json = json_decode($response);
+ return $json[0]->url_short;
+}
+
+function expandSinaUrl($short_url){
+ $apiKey='4098049467';
+ $apiUrl='http://api.t.sina.com.cn/short_url/expand.json?source='.$apiKey.'&url_short='.$short_url;
+ $curlObj = curl_init();
+ curl_setopt($curlObj, CURLOPT_URL, $apiUrl);
+ curl_setopt($curlObj, CURLOPT_RETURNTRANSFER, 1);
+ curl_setopt($curlObj, CURLOPT_SSL_VERIFYPEER, 0);
+ curl_setopt($curlObj, CURLOPT_HEADER, 0);
+ curl_setopt($curlObj, CURLOPT_HTTPHEADER, array('Content-type:application/json'));
+ $response = curl_exec($curlObj);
+ curl_close($curlObj);
+ $json = json_decode($response);
+ return $json[0]->url_long;
+}
+
+
+return shortenSinaUrl($xxurl);
+}
+
+function xurl($xxurl){
+$f = new SaeFetchurl();
+$content = $f->fetch('http://xurl.cn/api.php?url='.$xxurl);
+$data=object_array(json_decode($content));
+return ($data['info']);
+}
+//ZZB
+function zzb($xxurl){
+$url = "http://zzb.bz/panel/api/create/";
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_POST, TRUE);
+
+	$param_key = "NJR07"; // your api key
+	$param_url = $xxurl;  // your huge url
+	$param_password = "";
+	$param_description = "";
+	$array = array('key'=>$param_key, 'url'=>$param_url, 'password'=>$param_password, 'description'=>$param_description);
+	curl_setopt($ch, CURLOPT_POSTFIELDS , http_build_query($array));
+
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+	$response = curl_exec($ch);
+	curl_close($ch);
+   
+
+$data=object_array(json_decode($response));
+return ($data['Result']);
+}
+//Google
+function GoogleUrl($xxurl){
+ $apiKey = 'AIzaSyCKC1gSDQujgr94UgI_tVJahCGoOoIeuJ0'; 
+ $postData = array('longUrl' => $xxurl, 'key' => $apiKey);
+ $jsonData = json_encode($postData);
+ $curlObj = curl_init();
+ curl_setopt($curlObj, CURLOPT_URL, 'https://www.googleapis.com/urlshortener/v1/url');
+ curl_setopt($curlObj, CURLOPT_RETURNTRANSFER, 1);
+ curl_setopt($curlObj, CURLOPT_SSL_VERIFYPEER, 0);
+ curl_setopt($curlObj, CURLOPT_HEADER, 0);
+ curl_setopt($curlObj, CURLOPT_HTTPHEADER, array('Content-type:application/json'));
+ curl_setopt($curlObj, CURLOPT_POST, 1);
+ curl_setopt($curlObj, CURLOPT_POSTFIELDS, $jsonData);
+ $response = curl_exec($curlObj);
+ curl_close($curlObj);
+ $json = json_decode($response);
+ return $json->id;
+}
+
+if (isset($_POST['longurl']))
+  {
+  $xxurl = $_POST['longurl'] ; 
+    if (is_httpurl($xxurl) == 0){
+echo "<form onsubmit=\"return submitForm()\" action=\"index.php\" id=\"mainform\" name=\"mainform\" method=\"post\">\n";
+echo "<div id=\"urlboxcontainer\"><input type=\"text\" value=\"URL非法\" name=\"longurl\" size=\"55\" maxlength=\"5000\" class=\"urlbox\"></div>\n";
+echo "<input type=\"submit\" value=\"变短!\" class=\"submitbutton\">\n";
+echo "</form>";
+echo '<div>';
+      }
+    else{
+
+echo "<form onsubmit=\"return submitForm()\" action=\"index.php\" id=\"mainform\" name=\"mainform\" method=\"post\">\n";
+echo "<div id=\"urlboxcontainer\"><input type=\"text\" value=\"已经变短!\" name=\"url\" size=\"55\" maxlength=\"5000\" class=\"urlbox\"></div>\n";
+echo "<input type=\"submit\" value=\"变短!\" class=\"submitbutton\">\n";
+echo "</form>\n";
+echo "</div>\n";
+echo "<hr color=\"#987cb9\" width=\"80%\" size=\"1\" style=\"border: 1px dashed rgb(152, 124, 185);\"/>";
+echo baidu($xxurl);
+echo '<br />';
+echo bailu($xxurl);
+echo '<br />';
+echo bitly($xxurl);
+echo '<br />';
+echo isgd($xxurl);
+echo '<br />';
+echo lnkby($xxurl);
+echo '<br />';
+echo sinaweibo($xxurl);
+echo '<br />';
+echo zzb($xxurl);
+echo '<br />';
+echo vgd($xxurl);
+echo '<br />';
+echo GoogleUrl($xxurl);
+echo '<hr color="#987cb9" width="80%" size="1" style="border: 1px dashed rgb(152, 124, 185);"/>';
+      echo '<a href="api.html">API</a>已经开发，请阅读使用说明！';
+}
+  }
+else
+  {
+echo "<form onsubmit=\"return submitForm()\" action=\"index.php\" id=\"mainform\" name=\"mainform\" method=\"post\">\n";
+echo "<div id=\"urlboxcontainer\"><input type=\"text\" value=\"\" name=\"longurl\" size=\"55\" maxlength=\"5000\" class=\"urlbox\"></div>\n";
+echo "<input type=\"submit\" value=\"变短!\" class=\"submitbutton\">\n";
+echo "</form>";
+echo '<div>';
+  }
+?>
+</body></html>
