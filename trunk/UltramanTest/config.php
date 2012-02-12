baef94cdfdb838f6884756527f4e92ea9ecdcf8a
b5617777@gmail.com <b5617777@gmail.com@ed20f338-eeb6-4f25-9d68-1a73a7436480>
2012-02-12 00:35:10 +0000

    ultraman

---
+++ trunk/UltramanTest/config.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * : fanbingx@gmail.com
+ * : 2011-11-24 
+ */ 
+
+
+/*程序名称*/
+define('App_Name','Ultramantest');
+/*作者*/
+define('App_Author','XDash');
+/*版本*/
+define('App_Version','0.01');
+/*主页*/
+define('App_Homepage','http://www.fanbing.net');
+/*Email*/
+define('App_Email','fanbingx@gmail.com');
+
+
+// 新浪微博开放平台Key
+define( "WB_AKEY" , '4098049467' );
+define( "WB_SKEY" , 'fc6e86dbfbf1024142677b1f37815aaf' );
+
+
+
+
+
+?>
